ff7c93ae4ba4d44734d1a6398d978d261e903b2c
dolidev2 <dolidev2.0@gmail.com>
2023-09-17 10:40:47 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    date_default_timezone_set("Africa/Ouagadougou");
+    session_start();
+    define("URL", str_replace("index.php", "",
+        (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
+
+//    include controller object
+
+
+
+//    Mapping all routes
+    try {
+        if (empty($_GET['page'])) {
+            require "";
+        } else {
+            $url = explode("/", filter_var($_GET['page']), FILTER_SANITIZE_URL);
+            try {
+                switch ($url[0]){
+                    case 'home':
+
+                    break;
+                    }
+                }catch (Exception $e){
+                    echo $e->getMessage();
+                }
+            }
+    }catch (Exception $e){
+        echo $e->getMessage();
+        }
